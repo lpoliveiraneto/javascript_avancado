722818131d826f076368a4b103c0bb319b8985eb
Leonides Pires Oliveira Neto <leonidespires@gmail.com>
2020-06-09 21:37:51 -0300

    implementação da view de negociação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,14 +8,18 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
-    }  
+        this._negociacoesView.update(this._listaNegociacoes);
+    
+      }  
 
     adiciona(event) {
           event.preventDefault();
 
           //console.log(this._inputData.value);
           this._listaNegociacoes.adiciona(this._criaNegociacao());
+          this._negociacoesView.update(this._listaNegociacoes);
           this._limpaFormulario();
 
           //this._listaNegociacoes.negociacoes.length = 0;
@@ -27,8 +31,8 @@ class NegociacaoController {
     _criaNegociacao(){
       return  new Negociacao(
         DateHelper.textoParaData(this._inputData.value),
-        this._inputQuantidade,
-        this._inputValor
+        this._inputQuantidade.value,
+        this._inputValor.value
       )
     }
 
