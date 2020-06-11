f05721529561d558660dc50149a7f6cd3a0efbf0
Leonides Pires Oliveira Neto <leonidespires@gmail.com>
2020-06-10 21:49:03 -0300

    mensagem para usuário

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,10 +8,13 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
         this._negociacoesView.update(this._listaNegociacoes);
     
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
       }  
 
     adiciona(event) {
@@ -20,6 +23,9 @@ class NegociacaoController {
           //console.log(this._inputData.value);
           this._listaNegociacoes.adiciona(this._criaNegociacao());
           this._negociacoesView.update(this._listaNegociacoes);
+
+          this._mensagem.texto = 'Negociação adicionada com sucesso';
+          this._mensagemView.update(this._mensagem);
           this._limpaFormulario();
 
           //this._listaNegociacoes.negociacoes.length = 0;
