1f63c3f8727119095604157692d882fd85f11f3c
Leonides Pires Oliveira Neto <leonidespires@gmail.com>
2020-06-29 15:40:36 -0300

    código finalizado

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,11 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View{
 
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
 
-
-    _template(model){
+    template(model){
 
         return `
             <table class="table table-hover table-bordered">
@@ -41,9 +40,6 @@ class NegociacoesView {
         `;
     }
 
-    update(model){
-       this._elemento.innerHTML = this._template(model);
-    }
 }
 
 /*
