9532ca5e7b2abcf73ce61236d85870f94d864e00
Leonides Pires Oliveira Neto <leonidespires@gmail.com>
2020-07-17 23:04:37 -0300

    Implementação Bind e ProxyFactory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,32 +7,29 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes(this, function(model){
-
-          this._negociacoesView.update(model);
-        });
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-    
-        this._mensagem = new Mensagem();
+
+        this._listaNegociacoes = new Bind (
+          new ListaNegociacoes(),
+          this._negociacoesView,
+          ['adiciona', 'esvazia']
+          );
+
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        
+        this._mensagem = new Bind(
+          new Mensagem(),
+          this._mensagemView,
+          ['texto']
+        );
       }  
 
     adiciona(event) {
           event.preventDefault();
-
-          //console.log(this._inputData.value);
           this._listaNegociacoes.adiciona(this._criaNegociacao());
-          //this._negociacoesView.update(this._listaNegociacoes);
-
           this._mensagem.texto = 'Negociação adicionada com sucesso';
-          this._mensagemView.update(this._mensagem);
           this._limpaFormulario();
-
-          //this._listaNegociacoes.negociacoes.length = 0;
-          //this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
           console.log(this._listaNegociacoes.negociacoes);
 
     }
@@ -47,17 +44,13 @@ class NegociacaoController {
 
     apaga(){
       this._listaNegociacoes.esvazia();
-      //this._negociacoesView.update(this._listaNegociacoes);
-    
       this._mensagem.texto = 'Negociações apagadas';
-      this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario(){
       this._inputData.value = '';
       this._inputQuantidade.value = 1;
       this._inputValor.value = 0.0;
-
       this._inputData.focus();
     }
   }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,16 +1,12 @@
 
 class ListaNegociacoes{
 
-    constructor(contexto, armadilha){
+    constructor(){
         this._negociacoes = [];
-        this._armadilha = armadilha;
-        this._contexto = contexto;
     }
 
     adiciona(negociacao){
-        this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._negociacoes.push(negociacao);        
     }
 
     get negociacoes(){
@@ -19,7 +15,5 @@ class ListaNegociacoes{
 
     esvazia(){
         this._negociacoes = [];
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
