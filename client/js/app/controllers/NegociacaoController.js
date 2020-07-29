249126f5a93244efd39838979f59225d50c67be5
Leonides Pires Oliveira Neto <leonidespires@gmail.com>
2020-07-29 11:41:05 -0300

    importando negociações assíncronas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,36 +2,44 @@ class NegociacaoController {
 
     constructor(){
       
-        let $ = document.querySelector.bind(document)
+      let $ = document.querySelector.bind(document)
 
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
+      this._inputData = $('#data');
+      this._inputQuantidade = $('#quantidade');
+      this._inputValor = $('#valor');
         
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
-        this._listaNegociacoes = new Bind (
-          new ListaNegociacoes(),
-          this._negociacoesView,
-          ['adiciona', 'esvazia']
-          );
-
-        this._mensagemView = new MensagemView($('#mensagemView'));
+      this._listaNegociacoes = new Bind (
+        new ListaNegociacoes(),
+        new NegociacoesView($('#negociacoesView')),
+        'adiciona', 'esvazia'
+      );
         
-        this._mensagem = new Bind(
-          new Mensagem(),
-          this._mensagemView,
-          ['texto']
-        );
-      }  
+      this._mensagem = new Bind(
+        new Mensagem(),
+        new MensagemView($('#mensagemView')),
+        'texto'
+      );
+    }  
 
     adiciona(event) {
-          event.preventDefault();
-          this._listaNegociacoes.adiciona(this._criaNegociacao());
-          this._mensagem.texto = 'Negociação adicionada com sucesso';
-          this._limpaFormulario();
-          console.log(this._listaNegociacoes.negociacoes);
+      event.preventDefault();
+      this._listaNegociacoes.adiciona(this._criaNegociacao());
+      this._mensagem.texto = 'Negociação adicionada com sucesso';
+      this._limpaFormulario();
+      console.log(this._listaNegociacoes.negociacoes);
 
+    }
+
+    importaNegociacoes(){
+     let service = new NegociacaoService();
+     service.obterNegociacaoDasemana((erro, negociacoes) => {
+       if(erro){
+         this._mensagem.texto = erro;
+       }
+
+       negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+       this._mensagem.texto = 'Negociações importadas com sucesso';
+     });
     }
 
     _criaNegociacao(){
